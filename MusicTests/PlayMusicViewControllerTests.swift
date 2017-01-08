00b01f99f85b0fcd4336904b53449bfabf8a2e72
Thomas <TNoble@Thomass-Mac.local>
2017-01-08 18:27:18 +0100

    Test Music + func getAllMusic

---
+++ MusicTests/PlayMusicViewControllerTests.swift
@@ -0,0 +1,26 @@
+//
+//  PlayMusicViewControllerTests.swift
+//  Music
+//
+//  Created by Thomas on 08/01/17.
+//  Copyright © 2017 Brezeler. All rights reserved.
+//
+
+import XCTest
+@testable import Music
+
+
+class PlayMusicViewControllerTests: XCTestCase {
+    
+    
+    func testDownloadFileFromURL() {
+        //TODO
+    }
+    
+    
+    func testLoadImageFromUrl() {
+        //TODO
+    }
+    
+    
+}
